ab15e4755f7f6a7daf76de721884c86223f2d711
mykolaAwery <mykola.n@awery.aero>
2025-03-01 21:54:57 +0200

    add CreateVideoCommand

--- src/UI/Http/Rest/Internal/Controller/V1/Video/CreateVideoController.php
+++ src/UI/Http/Rest/Internal/Controller/V1/Video/CreateVideoController.php
@@ -4,70 +4,85 @@ declare(strict_types=1);
 
 namespace App\UI\Http\Rest\Internal\Controller\V1\Video;
 
-use App\Application\Command\User\Create\CreateUserCommand;
+use App\Application\Command\Video\Create\CreateVideoCommand;
 use App\UI\Http\Rest\Internal\Controller\CommandController;
-use App\UI\Http\Rest\Internal\DTO\Users\CreateUserRequest;
+use App\UI\Http\Rest\Internal\DTO\Video\CreateVideoRequest;
 use OpenApi\Attributes as OA;
-use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\HandledStamp;
 
-#[OA\Tag(name: 'User')]
+#[OA\Tag(name: 'Video')]
 final class CreateVideoController extends CommandController
 {
-    public string $dtoClass = CreateUserRequest::class;
+    public string $dtoClass = CreateVideoRequest::class;
 
     #[OA\Post(
-        summary: 'Create user',
+        summary: 'Create video',
         security: [['Bearer' => []]],
         responses: [
-            new OA\Response(response: Response::HTTP_CREATED, description: 'User created successfully'),
+            new OA\Response(response: Response::HTTP_CREATED, description: 'video created successfully'),
             new OA\Response(response: Response::HTTP_BAD_REQUEST, description: 'Bad request'),
             new OA\Response(response: Response::HTTP_CONFLICT, description: 'Conflict'),
             new OA\Response(response: Response::HTTP_INTERNAL_SERVER_ERROR, description: 'Internal server error')
         ]
     )]
     #[OA\Parameter(
-        name: 'name',
-        description: 'Name',
+        name: 'title',
+        description: 'Title',
         in: 'query',
         required: true,
         schema: new OA\Schema(type: 'string')
     )]
     #[OA\Parameter(
-        name: 'email',
-        description: 'Email',
+        name: 'description',
+        description: 'Description',
         in: 'query',
-        required: true,
+        required: false,
         schema: new OA\Schema(type: 'string')
     )]
     #[OA\Parameter(
-        name: 'password',
-        description: 'Password',
+        name: 'filePath',
+        description: 'filePath',
         in: 'query',
-        required: true,
+        required: false,
         schema: new OA\Schema(type: 'string')
+    )]
+    #[OA\Parameter(
+        name: 'thumbnailPath',
+        description: 'thumbnailPath',
+        in: 'query',
+        required: false,
+        schema: new OA\Schema(type: 'string')
+    )]
+    #[OA\Parameter(
+        name: 'duration',
+        description: 'duration',
+        in: 'query',
+        required: false,
+        schema: new OA\Schema(type: 'integer')
     )]
     public function __invoke(Request $request, MessageBusInterface $messageBus): JsonResponse
     {
-        $name = $request->get('name');
-        $email = $request->get('email');
-        $password = $request->get('password');
-        //todo fix !!!!!
-        $command = new CreateUserCommand(
-            name: $name,
-            email: $email,
-            password: $password,
+        $title = $request->get('title');
+        $description = $request->get('description');
+        $filePath = $request->get('filePath');
+        $thumbnailPath = $request->get('thumbnailPath');
+        $duration = $request->get('duration', 0);
+        $command = new CreateVideoCommand(
+            title: $title,
+            description: $description,
+            filePath: $filePath,
+            thumbnailPath: $thumbnailPath,
+            duration: $duration,
         );
         $envelope = $messageBus->dispatch($command);
         $handledStamp = $envelope->last(HandledStamp::class);
         if (!$handledStamp) {
             throw new \RuntimeException('No handler was found for this query or handler failed to execute.');
         }
-        return new JsonResponse(['id' => $uuid], Response::HTTP_CREATED);
-
+        return new JsonResponse([], Response::HTTP_CREATED);
     }
 }
